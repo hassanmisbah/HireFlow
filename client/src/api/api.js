8d758a880c83e7919a713f1b9715c6e3a715e698
Hassan Misbah <hassanmisbah009@gmail.com>
2026-09-03 19:52:59 +0500

    Fix backend API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hire-flow-gules-six.vercel.app/",
+  baseURL: "https://hire-flow-gules-six.vercel.app/api",
 });
 
 export default api;
