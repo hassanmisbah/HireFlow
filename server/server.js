f520d3e1b8a25ace640a408cdb0bda0a0c62bbf2
Hassan Misbah <hassanmisbah009@gmail.com>
2026-09-03 19:26:08 +0500

    Prepare backend for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,24 +1,20 @@
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 require("dotenv").config();
+
 const authRoutes = require("./routes/authRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
-
-const PORT = 5000;
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("HireFlow Backend is Running");
 });
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
@@ -28,12 +24,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected Successfully");
-
-    app.listen(PORT, () => {
-      console.log(`HireFlow server running on http://localhost:${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB Connection Failed");
     console.error(error.message);
   });
+
+module.exports = app;
